644b52ad2e6d4695a68e5a93fde00f8e1b4bef65
FannyEste <estefannymorana@gmail.com>
2025-02-09 23:32:36 +0100

    move images to public

--- src/sections/Blog.jsx
+++ src/sections/Blog.jsx
@@ -10,7 +10,7 @@ export const Blog = () => {
             {/* 🔹 Wrap all Cards inside a Two-Column Grid Container */}
             <div className="blog-container">
                 <Card 
-                    imageSource="../assets/blog1.svg"
+                    imageSource="/assets/blog1.svg"
                     borderColor="orange"
                     cardTitle="How to stop being scared: Learning to love code"
                     cardDescription="Writing code is less about “just working.” Instead, it’s much more about creativity. It’s similar to creative writing in that the author architecting a story must use an assortment of tools and components (i.e., content) in a way to satisfy a particular objective."
@@ -18,7 +18,7 @@ export const Blog = () => {
                     date="July 2023"
                 />
                 <Card 
-                    imageSource="../assets/blog2.svg"
+                    imageSource="/assets/blog2.svg"
                     borderColor="orange"
                     cardTitle="How to stop being scared: Learning to love code"
                     cardDescription="Writing code is less about “just working.” Instead, it’s much more about creativity. It’s similar to creative writing in that the author architecting a story must use an assortment of tools and components (i.e., content) in a way to satisfy a particular objective."
@@ -26,7 +26,7 @@ export const Blog = () => {
                     date="July 2023"
                 />
                 <Card 
-                    imageSource="../assets/blog3.svg"
+                    imageSource="/assets/blog3.svg"
                     borderColor="orange"
                     cardTitle="How to stop being scared: Learning to love code"
                     cardDescription="Writing code is less about “just working.” Instead, it’s much more about creativity. It’s similar to creative writing in that the author architecting a story must use an assortment of tools and components (i.e., content) in a way to satisfy a particular objective."
@@ -34,7 +34,7 @@ export const Blog = () => {
                     date="July 2023"
                 />
                 <Card 
-                    imageSource="../assets/blog4.svg"
+                    imageSource="/assets/blog4.svg"
                     borderColor="orange"
                     cardTitle="How to stop being scared: Learning to love code"
                     cardDescription="Writing code is less about “just working.” Instead, it’s much more about creativity. It’s similar to creative writing in that the author architecting a story must use an assortment of tools and components (i.e., content) in a way to satisfy a particular objective."
