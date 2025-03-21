c362d0253314610255272ed58a38031f824c3386
FannyEste <estefannymorana@gmail.com>
2025-03-21 23:42:16 +0100

    fixed responsiveness for contact page

--- src/sections/Tech.jsx
+++ src/sections/Tech.jsx
@@ -5,7 +5,7 @@ import { Grid } from "../ui/Grid";
 export const Tech = () => (
     <Grid background="orange" sectionType="tech">
         <TextBox text="Tech" textType="h1" sectionType="tech" />
-        <TextBox text="HTML, CSS, Flexbox, JavaScript, ES6, JSX, React, React Hooks, Node.js, Mongo DB, Web Accessibility, APIs, mob-programming, pair-programming, GitHub. " textType="h3" sectionType="tech" />
+        <TextBox text="HTML5, CSS3, JavaScript ES6, React, Styled, Components, GitHub, Atom, Postman, Adobe Photoshop, Adobe Illustrator, Figma, Keynote, Slack, Node.js, Design Lead, Branding, Strategy, Process Design, Concept Development, Agile Methodology, Hyper Island Toolbox. " textType="h3" sectionType="tech" />
     </Grid>
 )
 
